89b4d23be603050203edf1e772631e2644fc686a
b-fatma <fz.boulgheraif@gmail.com>
2025-08-17 18:01:38 +0100

    readme

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from QLearning import QLearningAgent
 
 if __name__ == "__main__":
     env = WumpusWorld()
-    print(env.create_reward_table())
+    reward_table = env.create_reward_table()
+
     agent = QLearningAgent(env)
     Q = agent.train()
-    print(Q)
+
     policy = agent.get_policy()
     print(policy)
